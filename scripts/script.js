0e283a6f12659fe287837ab839bc5d7054166e72
Onur Kalkan <onurkalkanv84@hotmail.com>
2019-09-16 15:46:04 +0300

    16 Sept 19 1545

--- scripts/script.js
+++ scripts/script.js
@@ -3,9 +3,12 @@ const Patches = require('Patches');
 const Diagnostics = require('Diagnostics');
 const CameraInfo = require('CameraInfo');
 
-
 const isR1 = CameraInfo.isRecordingVideo.monitor();
 Patches.setPulseValue('myPulse', isR1);
+
+const isSC = CameraInfo.isCapturingPhoto.monitor();
+Patches.setPulseValue('screenshot', isSC);
+
 
 
 
